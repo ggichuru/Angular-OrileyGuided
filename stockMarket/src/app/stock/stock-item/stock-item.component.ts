e184eccff1076fcc6ad8caa84c25daa33540479f
ggichuru.dev <ggichuru.dev@gmail.com>
2020-06-18 15:06:32 +0300

    NgClass and Ngstyle
    Angular directives, Ngclass and ngstyle binding

--- stockMarket/src/app/stock/stock-item/stock-item.component.ts
+++ stockMarket/src/app/stock/stock-item/stock-item.component.ts
@@ -8,15 +8,31 @@ import { Stock } from '../../model/stock';
 })
 export class StockItemComponent implements OnInit {
   public stock: Stock;
+  public stockClasses;
+  public stockStyles;
 
   constructor() { }
 
   ngOnInit() {
-    this.stock = new Stock('New Stock Company', 'NSC', 80, 90);
+    this.stock = new Stock('New Stock Company', 'NSC', 78, 90);
+    const diff = (this.stock.price / this.stock.previousPrice) - 1;
+    const largeChange = Math.abs(diff) > 0.01;
+
+    this.stockClasses = {
+      positive : this.stock.isPositiveChange(),
+      negative : !this.stock.isPositiveChange(),
+      'large-change' : largeChange,
+      'small-change' : !largeChange,
+    };
+
+    this.stockStyles = {
+      color : this.stock.isPositiveChange() ? 'green' : 'red',
+      'font-size' : largeChange ? '1.8em' : '0.8em'
+    };
   }
 
   toogleFavorite(event) {
-    console.log('We are toogling a favorite stock!', event);
+    console.log('We are toggling a favorite stock!', event);
     this.stock.favorite = !this.stock.favorite;
   }
 
