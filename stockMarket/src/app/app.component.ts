25009ed82d2903871b90921758d76cdb8291e2d5
ggichuru.dev <ggichuru.dev@gmail.com>
2020-07-09 23:50:23 +0300

    Interface and functions

--- stockMarket/src/app/app.component.ts
+++ stockMarket/src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  SimpleChanges,
+  OnChanges,
+  OnDestroy,
+  DoCheck,
+  AfterViewChecked,
+  AfterViewInit,
+  AfterContentChecked,
+  AfterContentInit} from '@angular/core';
 import {Stock} from './model/stock';
 
 @Component({
@@ -6,18 +16,59 @@ import {Stock} from './model/stock';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy,
+                                    OnChanges, DoCheck,
+                                    AfterContentInit, AfterContentChecked,
+                                    AfterViewInit, AfterViewChecked {
   title = 'stockMarket';
 
+
   public stockObj: Stock;
+  private counter = 1;
 
   ngOnInit() {
-    this.stockObj = new Stock('Test Set Test', 'TST', 455, 45);
+    this.stockObj = new Stock('Test Set Test - ' + this.counter++ , 'TST', 455, 45);
     this.stockObj = new Stock('Stock Is A Stock', 'SIAS', 3455, 322);
   }
 
   onToggleFavorite(stock: Stock) {
     console.log('Favorite for stock ', stock, 'was triggered');
     this.stockObj.favorite = !this.stockObj.favorite;
+  }
+
+  changeStockObject() {
+    this.stockObj = new Stock('Test Set Test - ' + this.counter++ , 'TST', 455, 45);
+  }
+  changeStockPrice() {
+    this.stockObj.price += 10;
+  }
+
+  ngAfterViewInit(): void {
+    console.log('App component - After view Init');
+  }
+  ngAfterViewChecked(): void {
+    console.log('App component - After View Checked');
+  }
+  ngAfterContentInit(): void {
+    console.log('App component - After Content Init');
+  }
+  ngAfterContentChecked(): void {
+    console.log('App component - After Content Checked');
+  }
+
+  ngDoCheck(): void {
+    console.log('App component - Do Check');
+  }
+
+  ngOnDestroy(): void {
+    console.log('App Component - On Destroy');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('App Component - On Changes', changes);
+  }
+
+  testMethod() {
+    console.log('Test Method in AppComponent Triggered.');
   }
 }
--- stockMarket/src/app/stock/stock-item/stock-item.component.ts
+++ stockMarket/src/app/stock/stock-item/stock-item.component.ts
@@ -1,12 +1,27 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
-import { Stock } from '../../model/stock';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  ChangeDetectionStrategy,
+  Component, DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output, SimpleChanges
+} from '@angular/core';
+import {Stock} from '../../model/stock';
 
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
-  styleUrls: ['./stock-item.component.css']
+  styleUrls: ['./stock-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class StockItemComponent implements OnInit {
+export class StockItemComponent implements OnInit, OnDestroy,
+  OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked {
   @Input() public stock: Stock;
   @Output() private toggleFavorite: EventEmitter<Stock>;
 
@@ -22,8 +37,36 @@ export class StockItemComponent implements OnInit {
   onToggleFavorite() {
     this.toggleFavorite.emit(this.stock);
   }
+  changeStockPrice() {
+    this.stock.price += 5;
+  }
 
   trackStockByCode(index, stock) {
     return stock.code;
+  }
+
+  ngAfterViewInit(): void {
+    console.log('App component - After view Init');
+  }
+  ngAfterViewChecked(): void {
+    console.log('App component - After View Checked');
+  }
+  ngAfterContentInit(): void {
+    console.log('App component - After Content Init');
+  }
+  ngAfterContentChecked(): void {
+    console.log('App component - After Content Checked');
+  }
+
+  ngDoCheck(): void {
+    console.log('App component - Do Check');
+  }
+
+  ngOnDestroy(): void {
+    console.log('App Component - On Destroy');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('App Component - On Changes', changes);
   }
 }
